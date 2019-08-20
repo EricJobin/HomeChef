9ca5fab76628e6fffa43339432802f1cc27cce57
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-08-20 14:38:28 -0400

    updated orders model file with foreign key association

--- models/orders.js
+++ models/orders.js
@@ -43,6 +43,11 @@ module.exports = function(sequelize,DataTypes){
 				allowNull: false
 		  }
 		});
+		Orders.belongsTo(models.Chefs,{
+			foreignKey: {
+				allowNull: false
+		  }	
+		});
 	  };
 	  
 	
