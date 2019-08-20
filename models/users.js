efcfbfeef7e643163734795214f164e0847d83a2
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-08-20 16:31:26 -0400

    updated user files with hashing and created login file to get information from login form to submit and chek

--- models/users.js
+++ models/users.js
@@ -1,10 +1,14 @@
+
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
+var bcrypt = require("bcryptjs");
 module.exports = function(sequelize,DataTypes){
 	var Users = sequelize.define("Users",{
 		userName: {
 			type: DataTypes.STRING,
 			allowNull: false,
+			unique: true,
 			validate: {
-				len: [1]
+				isEmail: true
 			}
 		},
 		password: {
@@ -24,5 +28,14 @@ module.exports = function(sequelize,DataTypes){
 		}
 		*/	
 	});
+	User.prototype.validPassword = function(password) {
+		return bcrypt.compareSync(password, this.password);
+	  };
+	  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+	  // In this case, before a User is created, we will automatically hash their password
+	  User.addHook("beforeCreate", function(user) {
+		user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+	  });
 	return Users;
 };
+
