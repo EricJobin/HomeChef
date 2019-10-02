eae0cbd60d205cbd148b22871729d6888739a44a
Eric Jobin <ejobin@gmail.com>
2019-10-02 18:45:08 -0400

    Have chefs rendering, and modal populating

--- public/js/customerPage.js
+++ public/js/customerPage.js
@@ -1,35 +1,35 @@
 // JS for rendering Chefs in Area and taking a Client order - EJ
 
 // ---------------  Dummy Data for dev & testing ----------------------------------
-var chefData = [
-	{
-		id: 4,
-		firstName: "Matty",
-		lastName: "Matheson",
-		emailChef: "matty@hotmail.com",
-		chefCity: "Toronto",
-		chefPic: "http://apparelmagazine.co.nz/restaurantandcafe/wp-content/uploads/sites/3/2019/06/Matty-Matheson.jpeg",
-		chefBio: "Matty The Flavor Lord Matheson was the executive chef of Parts & Labour, and regularly appeared on Vice's show Munchies and hosts Viceland's It's Suppertime.",
-	},
-	{
-		id: 3,
-		firstName: "Susur",
-		lastName: "Lee",
-		emailChef: "slee@hotmail.com",
-		chefCity: "slee@hotmail.com",
-		chefPic: "https://1843784937.rsc.cdn77.org/wp-content/uploads/2017/08/Susur-Lee-400x200.jpg",
-		chefBio: "Susur's awards include the prestigious CAA Five Diamond Award, Cannes, France; the American Academy of Hospitality Services' 5 Diamond Award and being named one of the Ten Chefs of the Millennium by Food & Wine.",
-	},
-	{
-		id: 6,
-		firstName: "Swedish",
-		lastName: "Chef",
-		emailChef: "bork@hotmail.com",
-		chefCity: "Toronto",
-		chefPic: "https://americanhistory.si.edu/sites/default/files/blog_files/a/6a00e553a80e10883401a3fcc7aa9f970b-800wi.png",
-		chefBio: "Børk! Børk! Børk!",
-	},
-];
+// var chefData = [
+// 	{
+// 		id: 4,
+// 		firstName: "Matty",
+// 		lastName: "Matheson",
+// 		emailChef: "matty@hotmail.com",
+// 		chefCity: "Toronto",
+// 		chefPic: "http://apparelmagazine.co.nz/restaurantandcafe/wp-content/uploads/sites/3/2019/06/Matty-Matheson.jpeg",
+// 		chefBio: "Matty The Flavor Lord Matheson was the executive chef of Parts & Labour, and regularly appeared on Vice's show Munchies and hosts Viceland's It's Suppertime.",
+// 	},
+// 	{
+// 		id: 3,
+// 		firstName: "Susur",
+// 		lastName: "Lee",
+// 		emailChef: "slee@hotmail.com",
+// 		chefCity: "slee@hotmail.com",
+// 		chefPic: "https://1843784937.rsc.cdn77.org/wp-content/uploads/2017/08/Susur-Lee-400x200.jpg",
+// 		chefBio: "Susur's awards include the prestigious CAA Five Diamond Award, Cannes, France; the American Academy of Hospitality Services' 5 Diamond Award and being named one of the Ten Chefs of the Millennium by Food & Wine.",
+// 	},
+// 	{
+// 		id: 6,
+// 		firstName: "Swedish",
+// 		lastName: "Chef",
+// 		emailChef: "bork@hotmail.com",
+// 		chefCity: "Toronto",
+// 		chefPic: "https://americanhistory.si.edu/sites/default/files/blog_files/a/6a00e553a80e10883401a3fcc7aa9f970b-800wi.png",
+// 		chefBio: "Børk! Børk! Børk!",
+// 	},
+// ];
 
 //------------------  End of Dummy Data Section ------------------------------------
 
@@ -37,36 +37,43 @@ var chefData = [
 var chefData=[];
 function getData(){
 
-$.get("/api/chefs/", function(data) {
-	console.log(data);
-	for(var i =0;i<data.length;i++){
-		var id = data[i].id;
-		var fn = data[i].firstName;
-		var ln = data[i].lastName;
-		var em = data[i].email;
-		var city = data[i].chefCity;
-		var pic = data[i].chefPic;
-		var bio = data[i].chefBio;
+	$.get("/api/chefs/", function(data) {
+		console.log(data);
+		// for(var i =0;i<data.length;i++){
+		// 	var id = data[i].id;
+		// 	var fn = data[i].firstName;
+		// 	var ln = data[i].lastName;
+		// 	var em = data[i].email;
+		// 	var city = data[i].chefCity;
+		// 	var pic = data[i].chefPic;
+		// 	var bio = data[i].chefBio;
 
-		var newchef = new chef(id,fn,ln,em,city,pic,bio);
-		function chef(id,first,last,email,city,pic,bio){
-			this.id =id;
-			this.firstName = first;
-			this.lastName =last;
-			this.emailChef = email;
-			this.chefCity = city;
-			this.chefPic = pic;
-			this.chefBio = bio;
+		// 	var newchef = new chef(id,fn,ln,em,city,pic,bio);
+		// 	function chef(id,first,last,email,city,pic,bio){
+		// 		this.id =id;
+		// 		this.firstName = first;
+		// 		this.lastName =last;
+		// 		this.emailChef = email;
+		// 		this.chefCity = city;
+		// 		this.chefPic = pic;
+		// 		this.chefBio = bio;
+		// 	}
+		// 	console.log(newchef);
+		// 	chefData.push(newchef);
+		// }
+		chefData = data;
+		// console.log(chefData)
+	}).then(
+		function(){
+			// console.log("in then statement");
+			renderChefs()
 		}
-		console.log(newchef);
-		chefData.push(newchef);
-	}
-});
+	)
 
-setTimeout( function(){
-	renderChefs();
-	// popUpChefModal();
-  }, 30 );
+	// setTimeout( function(){
+	// 	renderChefs();
+	// 	// popUpChefModal();
+	// }, 30 );
  
 }
 
@@ -92,26 +99,25 @@ function renderChefs() {
 }
 
 // Populates Modal with data from selected chef card
-function popUpChefModal(){
-	$(".chefCard").on("click", function(){
+function popUpChefModal(props){
+	$(".chefCard").on("click", function(klick){
+		console.log("I'm in the popUpChefModal function")
 
-		debugger;
 		$(".modal-title").empty();
 		$(".modal-body").empty();
 		$(".modal-footer").empty();
 
-		//viewid is the id of the chef from the database, chefNum is the place in the array the chef is at
+		// viewid is the id of the chef from the database, chefNum is the place in the array the chef is at
 		var viewid = parseInt(this.id.replace('chef',''));
 		console.log("id: "+viewid);
 		var chefNum = $(`#chef${viewid}`).data("arraynum");
 		var chefInfo= chefData[chefNum];
-
+		console.log(chefInfo);
 		
 		$(".modal-title").append(`${chefInfo.firstName} ${chefInfo.lastName}`);
 		$(".modal-body").append(`<img class="chefPic" src=${chefInfo.chefPic}><br>`);
 		$(".modal-body").append(`${chefInfo.chefBio}`);
 		$(".modal-footer").append(`<button type="button" class="btn btn-outline-success" data-dismiss="modal" id="bookChef" data-array=${chefNum} data-id="${viewid}">Choose this Chef</button>`);
-
 	});
 }
 
@@ -149,10 +155,16 @@ function submitOrder(order) {
 
 $(document).ready(function() {
 	getData();
-	// renderChefs();
-	popUpChefModal();
 	chooseChef();
 	getOrder();
+
+	//This works, but seems to run the popUpChefModal once for each 'mouseenter' occurance
+	//That stopped since I changed it from .on to .one, but still no idea why the above happened
+	$(document).one("mouseenter", ".chefCard", function(){
+		console.log("listening")
+	
+		popUpChefModal()
+	});
 	
 });
   
